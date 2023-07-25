8dcbc6a944f205392bd590f5fd79870412b48309
Vijay Kolar <vijayikolar@gmail.com>
2023-07-25 17:51:34 +0530

    deleting cabins

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,14 +1,23 @@
 import supabase from "./supabase.js";
 
 export async function getCabins() {
-  const { data, error } = await supabase
-    .from('cabins')
-    .select('*')
+  const { data, error } = await supabase.from("cabins").select("*");
 
   if (error) {
-    console.log("getCabins ", error)
-    throw new Error("Cabins could not be loaded")
+    console.log("getCabins ", error);
+    throw new Error("Cabins could not be loaded");
   }
 
-  return data
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { error, data } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.log("getCabins ", error);
+    throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
 }
